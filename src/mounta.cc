837cdb87b05b52b491234e9a355ca148c998ef3e
Robert Tiemann <r.tiemann@ta-hifi.com>
2015-12-07 16:09:38 +0100

    Use common code submodule to reduce code duplication.
    Change-Id: I7fe7fdb3b1014728c90c2842d9fe30f3575834bb

--- src/mounta.cc
+++ src/mounta.cc
@@ -32,6 +32,9 @@
 #include "messages.h"
 #include "versioninfo.h"
 
+ssize_t (*os_read)(int fd, void *dest, size_t count) = read;
+ssize_t (*os_write)(int fd, const void *buf, size_t count) = write;
+
 struct parameters
 {
     bool run_in_foreground;
