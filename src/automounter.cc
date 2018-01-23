TplusA/MounTA

commit 99e558530265ddef2c251fdef899ad1ecad7f2ca

Scan devices for available partitions on startup (#141).
Change-Id: I53e61384f9d3006db7f2ff99f2386918932051c6

src/automounter.cc
@@ -83,16 +83,6 @@ static std::string ensure_mountpoint_directory(const std::string &wd,
 {
     const auto *dev = volume.get_device();
 
-    if(dev->get_working_directory().empty())
-    {
-        std::ostringstream os;
-
-        os << wd << '/' << volume.get_device()->get_id();
-
-        if(os_mkdir_hierarchy(os.str().c_str(), true))
-            volume.set_device_working_dir(os.str());
-    }
-
     if(volume.get_state() == Devices::Volume::PENDING &&
        !dev->get_working_directory().empty())
     {
@@ -100,7 +90,7 @@ static std::string ensure_mountpoint_directory(const std::string &wd,
 
         os << dev->get_working_directory() << '/' << volume.get_index();
 
-        if(os_mkdir(os.str().c_str(), true))
+        if(os_mkdir_hierarchy(os.str().c_str(), true))
             return os.str();
     }
 
@@ -204,51 +194,12 @@ static void apply_volume_filter(Devices::Volume &vol)
     log_assert(vol.get_state() == Devices::Volume::PENDING);
 }
 
-void Automounter::Core::handle_new_device(const char *device_path)
+static void try_mount_volume(Devices::Volume &vol, const Devices::Device &dev,
+                             const std::string &working_directory,
+                             const Automounter::ExternalTools &tools,
+                             const Automounter::FSMountOptions &mount_options)
 {
-    log_assert(device_path != nullptr);
-
-    msg_info("New device: \"%s\"", device_path);
-
-    Devices::Volume *vol;
-    auto *dev = devman_.new_entry(device_path, vol);
-
-    if(dev == nullptr || vol == nullptr)
-    {
-        if(dev == nullptr)
-            msg_error(0, LOG_NOTICE, "Failed using device %s", device_path);
-
-        return;
-    }
-
-    bool is_device_announcement_needed = false;
-
-    switch(dev->get_state())
-    {
-      case Devices::Device::SYNTHETIC:
-        /* handled later when the device is really available */
-        return;
-
-      case Devices::Device::BROKEN:
-      case Devices::Device::REJECTED:
-        /* don't care about it anymore */
-        return;
-
-      case Devices::Device::PROBED:
-        apply_device_filter(*dev);
-
-        if(dev->get_state() != Devices::Device::OK)
-            return;
-
-        is_device_announcement_needed = true;
-
-        /* fall-through */
-
-      case Devices::Device::OK:
-        break;
-    }
-
-    switch(vol->get_state())
+    switch(vol.get_state())
     {
       case Devices::Volume::PENDING:
         /* try to mount below */
@@ -277,23 +228,97 @@ void Automounter::Core::handle_new_device(const char *device_path)
      * the device, but not the filtered volumes. If all available volumes are
      * filtered, then the device will still be visible, but appear empty.
      */
-    apply_volume_filter(*vol);
+    apply_volume_filter(vol);
 
-    const std::string mountpoint_path(ensure_mountpoint_directory(working_directory_, *vol));
+    const std::string mountpoint_path(ensure_mountpoint_directory(working_directory, vol));
 
-    if(is_device_announcement_needed)
-        announce_new_device(*dev);
-
-    if(vol->get_state() != Devices::Volume::PENDING)
+    if(vol.get_state() != Devices::Volume::PENDING)
         return;
 
     /*
      * None of the filters kicked in, so we'll try to mount the volume now.
      */
     if(!mountpoint_path.empty())
-        do_mount_volume(*vol, mountpoint_path, tools_, mount_options_);
+        do_mount_volume(vol, mountpoint_path, tools, mount_options);
     else
-        vol->set_unusable();
+        vol.set_unusable();
+}
+
+static void mount_all_pending_volumes(Devices::Device &dev,
+                                      const std::string &working_directory,
+                                      const Automounter::ExternalTools &tools,
+                                      const Automounter::FSMountOptions &mount_options)
+{
+    for(const auto &volinfo : dev)
+    {
+        if(volinfo.second == nullptr)
+            continue;
+
+        if(volinfo.second->get_state() == Devices::Volume::PENDING)
+            try_mount_volume(*volinfo.second, dev, working_directory,
+                             tools, mount_options);
+    }
+}
+
+void Automounter::Core::handle_new_device(const char *device_path)
+{
+    log_assert(device_path != nullptr);
+
+    msg_info("New device: \"%s\"", device_path);
+
+    Devices::Volume *vol;
+    bool have_probed_dev;
+    auto *dev = devman_.new_entry(device_path, vol, have_probed_dev);
+
+    if(dev == nullptr || vol == nullptr)
+    {
+        if(dev == nullptr)
+            msg_error(0, LOG_NOTICE, "Failed using device %s", device_path);
+
+        if(dev == nullptr || dev->empty())
+            return;
+    }
+
+    log_assert(dev != nullptr);
+
+    switch(dev->get_state())
+    {
+      case Devices::Device::SYNTHETIC:
+        /* handled later when the device is really available */
+        return;
+
+      case Devices::Device::BROKEN:
+      case Devices::Device::REJECTED:
+        /* don't care about it anymore */
+        return;
+
+      case Devices::Device::PROBED:
+        apply_device_filter(*dev);
+
+        if(dev->get_state() != Devices::Device::OK)
+            return;
+
+        /* fall-through */
+
+      case Devices::Device::OK:
+        break;
+    }
+
+    if(have_probed_dev)
+    {
+        std::ostringstream os;
+        os << working_directory_ << '/' << dev->get_id();
+        dev->set_working_directory(os.str());
+
+        announce_new_device(*dev);
+    }
+
+    if(have_probed_dev)
+        mount_all_pending_volumes(*dev, working_directory_,
+                                  tools_, mount_options_);
+    else if(vol != nullptr)
+        try_mount_volume(*vol, *dev, working_directory_,
+                         tools_, mount_options_);
 }
 
 static void try_unmount_volume(Devices::Volume &vol,

src/device_manager.cc
@@ -126,11 +126,13 @@ Devices::Device *Devices::AllDevices::find_root_device(const char *devlink)
 }
 
 Devices::Device *Devices::AllDevices::new_entry(const char *devlink,
-                                                Devices::Volume *&volume)
+                                                Devices::Volume *&volume,
+                                                bool &have_probed_containing_device)
 {
     log_assert(devlink != nullptr);
 
     volume = nullptr;
+    have_probed_containing_device = false;
 
     DevnameWithVolumeNumber data;
     if(!get_devname_with_volume_number(&data, devlink))
@@ -141,7 +143,8 @@ Devices::Device *Devices::AllDevices::new_entry(const char *devlink,
 
     Device *device =
         (data.volume_number_ == 0)
-        ? add_or_get_device(devlink, data.devname_, volinfo, have_volume_info)
+        ? add_or_get_device(devlink, data.devname_, volinfo, have_volume_info,
+                            have_probed_containing_device)
         : find_root_device(devlink);
 
     if(data.volume_number_ > 0 || have_volume_info)
@@ -164,7 +167,9 @@ Devices::Device *Devices::AllDevices::new_entry(const char *devlink,
     }
     else if(device != nullptr && data.volume_number_ == 0)
     {
-        device->probe();
+        if(!have_probed_containing_device)
+            have_probed_containing_device = device->probe();
+
         volume = device->lookup_volume_by_devname(data.devname_);
     }
 
@@ -277,7 +282,8 @@ Devices::Device *
 Devices::AllDevices::add_or_get_device(const char *devlink,
                                        const char *devname,
                                        struct osdev_volume_info &volinfo,
-                                       bool &have_info)
+                                       bool &have_info,
+                                       bool &have_probed_containing_device)
 {
     have_info = false;
 
@@ -294,9 +300,12 @@ Devices::AllDevices::add_or_get_device(const char *devlink,
     have_info = osdev_get_volume_information(devname, &volinfo);
 
     return mk_device(devices_,
-        [&devlink] (const Devices::ID &device_id)
+        [&devlink, &have_probed_containing_device]
+        (const Devices::ID &device_id)
         {
-            return new Devices::Device(device_id, devlink, true);
+            auto *d = new Devices::Device(device_id, devlink, true);
+            have_probed_containing_device = d->get_state() == Device::State::PROBED;
+            return d;
         });
 }
 

src/device_manager.hh
@@ -50,11 +50,14 @@ class AllDevices
     explicit AllDevices() {}
     ~AllDevices();
 
-    Device *new_entry(const char *devlink, Volume *&volume);
+    Device *new_entry(const char *devlink, Volume *&volume,
+                      bool &have_probed_containing_device);
 
-    const Device *new_entry(const char *devlink, const Volume *&volume)
+    const Device *new_entry(const char *devlink, const Volume *&volume,
+                            bool &have_probed_containing_device)
     {
-        return new_entry(devlink, const_cast<Devices::Volume *&>(volume));
+        return new_entry(devlink, const_cast<Devices::Volume *&>(volume),
+                         have_probed_containing_device);
     }
 
     using RemoveDeviceCallback = std::function<void(Devices::Device &)>;
@@ -75,7 +78,8 @@ class AllDevices
   private:
     Device *add_or_get_device(const char *devlink, const char *devname,
                               struct osdev_volume_info &volinfo,
-                              bool &have_info);
+                              bool &have_info,
+                              bool &have_probed_containing_device);
 
     Device *find_root_device(const char *devlink);
     Device *get_device_by_devlink(const char *devlink);

src/devices.cc
@@ -72,13 +72,12 @@ void Devices::Device::set_working_directory(const std::string &path)
     mountpoint_container_path_ = path;
 }
 
-void Devices::Device::probe()
+bool Devices::Device::probe()
 {
-    if(state_ == SYNTHETIC)
-        return do_probe();
+    return state_ == SYNTHETIC ? do_probe() : false;
 }
 
-void Devices::Device::do_probe()
+bool Devices::Device::do_probe()
 {
     log_assert(state_ == SYNTHETIC);
 
@@ -91,8 +90,10 @@ void Devices::Device::do_probe()
        !osdev_get_device_information(devlink_name_.c_str(), &devinfo))
     {
         state_ = BROKEN;
-        return;
+        return false;
     }
+
+    bool result = false;
 
     switch(devinfo.type)
     {
@@ -104,10 +105,13 @@ void Devices::Device::do_probe()
         root_hub_id_ = Devices::USBHubID(devinfo.usb.hub_id);
         hub_port_ = devinfo.usb.port;
         state_ = PROBED;
+        result = true;
         break;
     }
 
     osdev_free_device_information(&devinfo);
+
+    return result;
 }
 
 static int do_remove_mountpoint_directory(const char *path,

src/devices.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015  T+A elektroakustik GmbH & Co. KG
+ * Copyright (C) 2015, 2017  T+A elektroakustik GmbH & Co. KG
  *
  * This file is part of MounTA.
  *
@@ -220,7 +220,7 @@ class Device
 
     void accept() { state_ = OK; }
     void reject() { state_ = REJECTED; }
-    void probe();
+    bool probe();
 
     Volume *lookup_volume_by_devname(const char *devname) const;
     bool add_volume(Volume &volume);
@@ -228,11 +228,12 @@ class Device
     const std::string &get_working_directory() const { return mountpoint_container_path_; }
     void set_working_directory(const std::string &path);
 
+    bool empty() const { return volumes_.empty(); }
     decltype(volumes_)::const_iterator begin() const { return volumes_.begin(); };
     decltype(volumes_)::const_iterator end() const   { return volumes_.end(); };
 
   private:
-    void do_probe();
+    bool do_probe();
     void cleanup_fs(bool not_expecting_failure);
 };
 
@@ -328,11 +329,6 @@ class Volume
     const std::string &get_device_name() const { return devname_; }
 
     void reject() { state_ = REJECTED; }
-
-    void set_device_working_dir(const std::string &path) const
-    {
-        containing_device_.set_working_directory(path);
-    }
 
     const std::string &get_mountpoint() const { return mountpoint_path_; }
 

src/mounta.cc
@@ -205,6 +205,23 @@ static int setup_inotify_watch(FdEvents &ev, const char *path,
     return 0;
 }
 
+using CollectDevicesData =
+    std::pair<std::pair<Automounter::Core, GMainLoop *> &, const char *const>;
+
+static int collect_devices(const char *path, unsigned char dtype,
+                           void *user_data)
+{
+    auto &data = *static_cast<CollectDevicesData *>(user_data);
+
+    std::string full_path(data.second);
+    full_path += '/';
+    full_path += path;
+
+    handle_device_changes(FdEvents::NEW_DEVICE, full_path.c_str(), &data.first);
+
+    return 0;
+}
+
 static gboolean signal_handler(gpointer user_data)
 {
     g_main_loop_quit(static_cast<GMainLoop *>(user_data));
@@ -268,10 +285,27 @@ int main(int argc, char *argv[])
     if(dbus_setup(loop, parameters.connect_to_session_dbus, &event_data.first) < 0)
         return EXIT_FAILURE;
 
+    static const char watched_directory[] = "/dev/disk/by-id";
+
+    /* install inotify watch first to make sure are not losing anything */
     static FdEvents ev;
-    if(setup_inotify_watch(ev, "/dev/disk/by-id", event_data) < 0)
+    if(setup_inotify_watch(ev, watched_directory, event_data) < 0)
         return EXIT_FAILURE;
 
+    /* after the inotify watch has been installed, we check the directory of
+     * block devices for entries which have already there when we were started;
+     * we are not going to lose any inotify events, but events may occur while
+     * the directory is inspected, possibly leading to events for entries we've
+     * already seen */
+    {
+        CollectDevicesData data(std::ref(event_data), watched_directory);
+
+        if(os_foreach_in_path(watched_directory, collect_devices, &data) < 0)
+            return EXIT_FAILURE;
+    }
+
+    /* any inotify events already received from kernel, if any, will be
+     * processed within a GLib main loop */
     g_main_loop_run(loop);
 
     msg_info("Shutting down");

tests/test_device_manager.cc
@@ -136,11 +136,14 @@ new_device_with_expectations(const DevNames &device_names,
         add_device_probe_expectations(device_names.device_identifier, &fake_device_info);
 
     const Devices::Volume *volume;
-    const Devices::Device *const dev = devs->new_entry(device_names.device_identifier, volume);
+    bool have_probed_dev;
+    const Devices::Device *const dev =
+        devs->new_entry(device_names.device_identifier, volume, have_probed_dev);
 
     cppcut_assert_not_null(dev);
     cppcut_assert_equal(device_names.device_identifier, dev->get_devlink_name().c_str());
     cppcut_assert_equal(Devices::Device::PROBED, dev->get_state());
+    cppcut_assert_equal(expecting_device_probe, have_probed_dev);
 
     if(expecting_null_volume)
         cppcut_assert_null(volume);
@@ -201,12 +204,15 @@ new_volume_with_expectations(int idx, const DevNames &volume_names,
     mock_devices_os->expect_osdev_free_volume_information(&fake_info);
 
     const Devices::Volume *vol;
-    cppcut_assert_equal(expected_device, devs->new_entry(volume_names.device_identifier, vol));
+    bool have_probed_dev;
+    cppcut_assert_equal(expected_device,
+                        devs->new_entry(volume_names.device_identifier, vol, have_probed_dev));
     cppcut_assert_not_null(vol);
     cppcut_assert_equal(expected_device, vol->get_device());
     cppcut_assert_equal(volume_names.volume_label, vol->get_label().c_str());
     cppcut_assert_equal(volume_names.volume_fstype, vol->get_fstype().c_str());
     cppcut_assert_equal(expected_device_state, vol->get_device()->get_state());
+    cut_assert_false(have_probed_dev);
 
     cppcut_assert_equal(vol, expected_device->lookup_volume_by_devname(vol->get_device_name().c_str()));
 
@@ -230,10 +236,12 @@ new_volume_with_expectations(int idx, const DevNames &volume_names,
     mock_devices_os->expect_osdev_free_volume_information(&fake_info);
 
     const Devices::Volume *vol;
-    ret_device = devs->new_entry(volume_names.device_identifier, vol);
+    bool have_probed_dev;
+    ret_device = devs->new_entry(volume_names.device_identifier, vol, have_probed_dev);
     cppcut_assert_not_null(vol);
     cppcut_assert_equal(volume_names.volume_label, vol->get_label().c_str());
     cppcut_assert_equal(volume_names.volume_fstype, vol->get_fstype().c_str());
+    cut_assert_false(have_probed_dev);
 
     if(expecting_null_device)
         cppcut_assert_null(ret_device);

commit 529b93f54f6ef19277ecd95adcdeb417b4f80640

Fix mounting of certain (most?) USB devices (#729).
Bug was introduced in 99e558530265ddef2c251fdef899ad1ecad7f2ca which
added the initial device scan.

Change-Id: Idf447e38fb0d94bc84bb3e10bed25130540fd678

src/automounter.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015, 2016, 2017  T+A elektroakustik GmbH & Co. KG
+ * Copyright (C) 2015, 2016, 2017, 2018  T+A elektroakustik GmbH & Co. KG
  *
  * This file is part of MounTA.
  *
@@ -221,6 +221,9 @@ void Automounter::Core::handle_new_device(const char *device_path)
         /* fall-through */
 
       case Devices::Device::OK:
+        if(!have_probed_dev)
+            have_probed_dev = dev->get_working_directory().str().empty();
+
         break;
     }
 