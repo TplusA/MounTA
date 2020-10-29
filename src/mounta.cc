TplusA/MounTA

commit a5556723c58648fe8c457b808a1cd1f76bc74657

Add utf8 to vfat mount options.
Change-Id: I184b9425baae6bd09d84b2de9ea170e39efe68a1

src/mounta.cc
@@ -305,7 +305,7 @@ int main(int argc, char *argv[])
     static constexpr const char mount_options_default[] = "-o ro,noexec,nosuid,nodev,user";
 
     static constexpr const char mount_options_ext234[] = "-o errors=continue";
-    static constexpr const char mount_options_fatish[] = "-o umask=222";
+    static constexpr const char mount_options_fatish[] = "-o umask=222,utf8";
 
     static const Automounter::FSMountOptions
     mount_options(std::map<const std::string, const char *const>

commit 0b83d3ab8618531c19aecb6f31f3a6aecf54c7ab

hfsplus: Fix mount options (#1310).
Applied same fix for hfs. Problem was introduced in a555672;
apparently no one ever tested the patch with hfs and hfsplus.

Change-Id: I94f8d2e805656ec60ff52b483e73dc460a73c360

src/mounta.cc
@@ -313,6 +313,7 @@ int main(int argc, char *argv[])
 
     static constexpr const char mount_options_ext234[] = "-o errors=continue";
     static constexpr const char mount_options_fatish[] = "-o umask=222,utf8";
+    static constexpr const char mount_options_hfs[] = "-o umask=222";
 
     static const Automounter::FSMountOptions
     mount_options(std::map<const std::string, const char *const>
@@ -326,8 +327,8 @@ int main(int argc, char *argv[])
         { "msdos",   mount_options_fatish },
         { "vfat",    mount_options_fatish },
         { "ntfs",    mount_options_fatish },
-        { "hfs",     mount_options_fatish },
-        { "hfsplus", mount_options_fatish },
+        { "hfs",     mount_options_hfs },
+        { "hfsplus", mount_options_hfs },
         { "iso9660", nullptr },
     });
 